80cfd9272ff76fcf91848eee52780dd49f6f936b
Laqsha Asri <laqshaasri551@gmail.com>
2026-06-15 11:00:01 +0700

    Membuat nama user yg login

--- controller/authController.js
+++ controller/authController.js
@@ -1,31 +1,31 @@
-import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
-import { findUserByEmail } from '../models/usersModel.js';
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import { findUserByEmail } from "../models/usersModel.js";
 
-const JWT_SECRET = 'rahasia-super-aman'; // sebaiknya pindahkan ke .env
+const JWT_SECRET = "rahasia-super-aman"; // sebaiknya pindahkan ke .env
 
 export const login = (req, res) => {
   const { email, password } = req.body;
 
   if (!email || !password) {
-    return res.status(400).json({ message: 'Email dan password wajib diisi' });
+    return res.status(400).json({ message: "Email dan password wajib diisi" });
   }
 
   findUserByEmail(email, async (err, user) => {
     if (err) {
       console.error(err);
-      return res.status(500).json({ message: 'Terjadi kesalahan server' });
+      return res.status(500).json({ message: "Terjadi kesalahan server" });
     }
 
     if (!user) {
-      return res.status(401).json({ message: 'Email atau password salah' });
+      return res.status(401).json({ message: "Email atau password salah" });
     }
 
     try {
       const match = await bcrypt.compare(password, user.user_password);
 
       if (!match) {
-        return res.status(401).json({ message: 'Email atau password salah' });
+        return res.status(401).json({ message: "Email atau password salah" });
       }
 
       const token = jwt.sign(
@@ -35,22 +35,22 @@ export const login = (req, res) => {
           role: user.role,
         },
         JWT_SECRET,
-        { expiresIn: '1h' }
+        { expiresIn: "1h" },
       );
 
       res.json({
-        message: 'Login berhasil',
+        message: "Login berhasil",
         token,
         role: user.role,
         user: {
           id: user.user_id,
-          name: user.user_name,
+          user_name: user.user_name,
           email: user.user_email,
         },
       });
     } catch (e) {
       console.error(e);
-      res.status(500).json({ message: 'Terjadi kesalahan server' });
+      res.status(500).json({ message: "Terjadi kesalahan server" });
     }
   });
 };
