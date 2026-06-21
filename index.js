5d95cb8734dce6d24751717795d52b6ce7934e6a
Arrsssad <tubagusarsyad31@gmail.com>
2026-06-21 19:23:20 +0700

    cuman nambah http:// di index.js

--- index.js
+++ index.js
@@ -61,5 +61,5 @@ app.get("/admin/:page", (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Server running at ${hostname}:${port}`);
+  console.log(`Server running at http://${hostname}:${port}`);
 });
