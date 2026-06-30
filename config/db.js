e1f220e9c36ee91651370cca023c33e0d9cd9215
Laqsha Asri <laqshaasri551@gmail.com>
2026-06-30 22:15:41 +0700

    Tambah konfigurasi port

--- config/db.js
+++ config/db.js
@@ -5,6 +5,7 @@ import mysql2 from "mysql2";
 
 const db = mysql2.createConnection({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
