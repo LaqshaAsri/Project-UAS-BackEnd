a6ffca5017b1d38dcd3cbb8291860bb362561935
Laqsha Asri <laqshaasri551@gmail.com>
2026-06-30 22:30:05 +0700

    Tambah Port

--- config/db.js
+++ config/db.js
@@ -3,6 +3,13 @@ dotenv.config();
 
 import mysql2 from "mysql2";
 
+console.log({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  database: process.env.DB_NAME,
+});
+
 const db = mysql2.createConnection({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
