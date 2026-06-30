dfe2644f8817fda35e8abc17a43617a836d246ac
Laqsha Asri <laqshaasri551@gmail.com>
2026-06-30 22:36:14 +0700

    cek port

--- config/db.js
+++ config/db.js
@@ -7,6 +7,7 @@ console.log({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD ? "Ada" : "Tidak Ada",
   database: process.env.DB_NAME,
 });
 
